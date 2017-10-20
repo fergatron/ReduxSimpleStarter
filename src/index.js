42d3a21d908921b9f3e40ed72c9bdc4ef3f4eb14
Marc Ferguson <marc.ferguson@coxautoinc.com>
2017-10-20 16:02:09 -0400

    Refactored search bar.

--- src/index.js
+++ src/index.js
@@ -6,7 +6,11 @@ import SearchBar from './components/search-bar';
 const API_KEY = "AIzaSyAIdHnt2nq8ayq4RYKB_VPi4LCqoow6_Hs";
 
 const App = () => {
-    return <div>Hi!</div>;
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
