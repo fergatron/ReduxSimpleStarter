83adae3faab960fae228847e065187ca0569933e
Marc Ferguson <marc@fergytech.com>
2017-11-09 21:46:29 -0500

    Inital work in the new section.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import SearchBar from './components/search-bar';
-
-const API_KEY = "AIzaSyAIdHnt2nq8ayq4RYKB_VPi4LCqoow6_Hs";
-
 const App = () => {
     return (
         <div>
-            <SearchBar />
+          Hello World!
         </div>
     );
 }
