4e004ab5bc28ac9edb9ade740f6f2d3ba0df87ff
Marc Ferguson <marc@fergytech.com>
2017-12-19 18:58:04 -0500

    Add error style toggle to form.

--- src/components/posts-new.js
+++ src/components/posts-new.js
@@ -1,13 +1,78 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
 
 class PostsNew extends Component {
+  renderField(field) {
+    const { meta: {touched, error} } = field;
+    const className = `form-group ${touched && error ? 'has-danger' : ''}`;
+
+    return (
+      <div className={className}>
+        <label>{field.label}</label>
+        <input
+          type='text'
+          className='form-control'
+          {...field.input}
+        />
+        <div className='text-help'>
+          { touched ? error : '' }
+        </div>
+      </div>
+    );
+  }
+
+  onSubmit(values) {
+    console.log(values);
+  }
+
   render() {
+    const { handleSubmit } = this.props;
+
     return (
       <div>
         <h3>New Post</h3>
+        <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+          <Field
+            name='title'
+            label='Title'
+            component={this.renderField}
+          />
+          <Field
+            name='categories'
+            label='Categories'
+            component={this.renderField}
+          />
+          <Field
+            name='content'
+            label='Post Content'
+            component={this.renderField}
+          />
+          <button type='submit' className='btn btn-primary'>Submit</button>
+          <Link to='/' className='btn btn-danger'>Cancel</Link>
+        </form>
       </div>
     );
   }
 }
 
-export default PostsNew;
+function validate(values) {
+  const errors = {};
+
+  if (!values.title || values.title.length < 3) {
+    errors.title = "Enter a title that is at least 3 characters!";
+  }
+  if (!values.categories) {
+    errors.categories = "Enter some categories!";
+  }
+  if (!values.content) {
+    errors.content = "Enter some content.";
+  }
+
+  return errors;
+}
+
+export default reduxForm({
+  validate,
+  form: 'PostsNewForm'
+})(PostsNew);
